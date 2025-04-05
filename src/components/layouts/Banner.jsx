5f238c9790488cc9d3c6f7986718420ebd28da0e
Abdullah-Adeeb <www.adib2004@gmail.com>
2025-04-06 00:56:14 +0600

    dd

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const banner = "/src/assets/banner.png";
+const banner = "src/assets/banner.png";
 
 const Banner = () => {
   let [active, setActive] = useState(0);
