13098fe91124f53594e9bd0e1d6072744dbb4edf
Abdullah-Adeeb <www.adib2004@gmail.com>
2025-04-06 00:51:43 +0600

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import RootLayouts from "./components/layouts/RootLayouts";
-import HOME from "./components/pages/Home";
 import { Routes , Route } from "react-router-dom";
 import Shop from "./components/pages/Shop";
 import About from "./components/pages/About";
@@ -9,12 +8,13 @@ import Error from "./components/pages/Error";
 import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
 import MyAcoount from "./components/pages/MyAcoount";
+import Home from "./components/pages/HOME";
 function App() {
   return (
     <>
       <Routes>
         <Route element={<RootLayouts/>}>
-        <Route path="/" element={<HOME/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/shop" element={<Shop/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
